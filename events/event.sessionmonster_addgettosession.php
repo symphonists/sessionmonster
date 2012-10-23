85b5104e140faaf3f13f9f7761a02d7ba0fb8bd5
Vlad Ghita <vlad_micutul@yahoo.com>
2012-10-23 17:59:31 +0300

    Realease 1.1

--- events/event.sessionmonster_addgettosession.php
+++ events/event.sessionmonster_addgettosession.php
@@ -1,68 +1,87 @@
 <?php
 
-	if(!defined('__IN_SYMPHONY__')) die('<h2>Error</h2><p>You cannot directly access this file</p>');
 
-	Final Class eventSessionmonster_addgettosession extends Event{
+
+	if( !defined( '__IN_SYMPHONY__' ) ) die('<h2>Error</h2><p>You cannot directly access this file</p>');
+
+
+
+	Final Class eventSessionmonster_addgettosession extends Event
+	{
 
 		public static function about(){
-					
 			return array(
-						 'name' => 'Session Monster: Add GET variable to session',
-						 'author' => array('name' => 'Alistair Kearney',
-										   'website' => 'http://www.pointybeard.com',
-										   'email' => 'alistair@pointybeard.com'),
-						 'version' => '1.0',
-						 'release-date' => '2008-05-12',						 
-					);						 
+				'name' => 'Session Monster: Add GET variable to session',
+				'author' => array('name' => 'Alistair Kearney',
+					'website' => 'http://www.pointybeard.com',
+					'email' => 'alistair@pointybeard.com'),
+				'version' => '1.0',
+				'release-date' => '2008-05-12',
+			);
 		}
-				
+
 		public function load(){
-			if(is_array($_GET) && !empty($_GET)) return $this->__trigger();
-			return NULL;
+			if( is_array( $_GET ) && !empty($_GET) ) return $this->__trigger();
+
+			return null;
 		}
 
 		public static function documentation(){
-			
 			return '
 		
 			<h3>Usage</h3>
 			
 			<p>Any GET parameters, i.e key & value pairs in the URL such as <code>?var1=hello</code>, will be added to the session, which is then available via the "Show Session Parameter" Data Source. XML after adding or removing variables look like so:</p>
-			
+
 			<pre class="XML"><code>&lt;session-monster>
-	&lt;item name="var1" action="added">one&lt;/item>
-	&lt;item name="var2" action="removed"/>
+	&lt;foo>one&lt;/foo>
+	&lt;bar>
+		&lt;baz>two&lt;/baz>
+		&lt;item-0>three&lt;/item-0>
+		&lt;item-2>four&lt;/item-2>
+	&lt;/bar>
 &lt;/session-monster></code></pre>
-			
-			<p>The corresponding URL for the above XML looks like <code>?var1=one&amp;var2=</code>. Notice that to remove an item, you set it as empty.</p>
-			
+
+			<p>The corresponding URL for the above XML looks like <code>?foo=one&amp;bar[baz]=two&amp;bar[]=three&bar[2]=four</code>. Notice that to remove an item, you set it as empty.</p>
 			';
 
 		}
-		
+
 		protected function __trigger(){
-			
-			session_start();			
-			$xml = new XMLElement('session-monster');
-			$exclude = array('symphony-page', 'Debug', 'debug', 'profile');
-			$count = 0;
-			
-		    foreach($_GET as $key => $val){
-		        if(!in_array($key, $exclude)){
-					$_SESSION[__SYM_COOKIE_PREFIX__ . '-sessionmonster'][$key] = $val;
-		        	$xml->appendChild(new XMLElement('item', $val, array('name' => $key, 'action' => (strlen($val) > 0 ? 'added' : 'removed'))));
-					$count++;
-		        }
-		    }
-			if(is_array($_SESSION[__SYM_COOKIE_PREFIX__ . '-sessionmonster'])) {
-				foreach($_SESSION[__SYM_COOKIE_PREFIX__ . '-sessionmonster'] as $key => $val){
-					if (!in_array($key, $exclude)) {
-						Frontend::Page()->_param['sessionmonster-'.$key] = $val;		
-					}					
+			session_start();
+
+			$exclude = array('symphony-page', 'Debug', 'debug', 'profile', 'XDEBUG_SESSION_START');
+
+			foreach( $_GET as $key => $val ){
+				if( !in_array( $key, $exclude ) ){
+					$this->process( $_SESSION['sessionmonster'], $key, $val );
 				}
 			}
-		    return ($count == 0 ? NULL : $xml);
-			
+
+			return '';
 		}
+
+		private function process(&$result, $key, $val){
+
+			// go deeper
+			if( is_array( $val ) ){
+				foreach( $val as $k => $v ){
+					$this->process( $result[$key], $k, $v );
+				}
+			}
+
+			// add
+			else if( !empty($val) ){
+				$result[$key] = $val;
+			}
+
+			// remove
+			else if( is_array( $result ) ){
+				if( isset($result[$key]) ){
+					unset($result[$key]);
+				}
+			}
+		}
+
 	}
 
